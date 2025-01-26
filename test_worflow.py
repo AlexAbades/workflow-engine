db9ba37140d0934a16e1f5809da9a6314f6091b0
AlexAbades <abadesalex31@gmail.com>
2025-01-26 18:05:46 +0100

    Link & tests update task state

--- test_worflow.py
+++ test_worflow.py
@@ -1,31 +1,87 @@
 import unittest
-from workflow import WorkFlow, Task, StateStatus
+from workflow import Link, WorkFlow, Task, StateStatus
+
 
 class Test_TestWorkFlow(unittest.TestCase):
 
     def test_worfkflow_test(self):
         self.assertTrue(True)
-        
-    
+
     def test_worflow_add_task(self):
 
         onboarding = WorkFlow()
-        
+
         tasks = ["Sign Up", "Verify Email"]
-        
+
         onboarding.add_task(tasks[0])
         onboarding.add_task(tasks[1])
-        
+
         self.assertEqual(onboarding._tasks, tasks)
-        
-        
-    def test_worfkflow_state_on_changed_task_state(self):
-        onboarding = WorkFlow() 
-        
+
+    def test_worfkflow_state_changes_on_changed_task_state(self):
+        onboarding = WorkFlow()
+
         onboarding.add_task(Task("Sign Up", onboarding))
         onboarding.add_task(Task("Verify Email", onboarding))
-        
-        onboarding.tasks[0].change_state(StateStatus.IN_PROGRESS)
-        
+
+        onboarding.tasks[0].change_task_state(StateStatus.IN_PROGRESS)
+
         self.assertEqual(onboarding.state, StateStatus.IN_PROGRESS)
-        
+
+    def test_workflow_state_is_completed_when_all_tasks_are_completed(self):
+        onboarding = WorkFlow()
+
+        onboarding.add_task(Task("Sign Up", onboarding))
+        onboarding.add_task(Task("Verify Email", onboarding))
+
+        onboarding.tasks[0].change_task_state(StateStatus.COMPLETED)
+        onboarding.tasks[1].change_task_state(StateStatus.COMPLETED)
+
+        self.assertEqual(onboarding.state, StateStatus.COMPLETED)
+
+    def test_workflow_state_is_pending_when_all_tasks_are_pending(self):
+        onboarding = WorkFlow()
+
+        onboarding.add_task(Task("Sign Up", onboarding))
+        onboarding.add_task(Task("Verify Email", onboarding))
+
+        self.assertEqual(onboarding.state, StateStatus.PENDING)
+
+    def test_workflow_state_is_in_progress_when_one_task_is_in_progress(self):
+        onboarding = WorkFlow()
+
+        onboarding.add_task(Task("Sign Up", onboarding))
+        onboarding.add_task(Task("Verify Email", onboarding))
+
+        onboarding.tasks[0].change_task_state(StateStatus.IN_PROGRESS)
+
+        self.assertEqual(onboarding.state, StateStatus.IN_PROGRESS)
+
+    def test_workflow_state_is_in_progress_when_all_tasks_are_completed_except_one(
+        self,
+    ):
+        onboarding = WorkFlow()
+
+        onboarding.add_task(Task("Sign Up", onboarding))
+        onboarding.add_task(Task("Verify Email", onboarding))
+
+        onboarding.tasks[0].change_task_state(StateStatus.COMPLETED)
+
+        self.assertEqual(onboarding.state, StateStatus.IN_PROGRESS)
+
+    def test_target_task_state_changes_on_source_task_change_state(self):
+        onboarding = WorkFlow()
+
+        sign_up = Task("sign Up", onboarding)
+        verify_email = Task("Verify Email", onboarding)
+
+        sign_up.add_link(
+            Link(sign_up, StateStatus.COMPLETED, verify_email, StateStatus.IN_PROGRESS)
+        )
+
+        onboarding.add_task(sign_up)
+        onboarding.add_task(verify_email)
+
+        sign_up.change_task_state(StateStatus.COMPLETED)
+
+        self.assertEqual(verify_email.state, StateStatus.IN_PROGRESS)
--- workflow.py
+++ workflow.py
@@ -25,21 +25,21 @@ class WorkFlow:
         return self._tasks
 
     # If a task state is changed, the workflow should be notified:
-    # - All tasks are pending: Worflow is pending
-    # - At least one task is in progress: Workflow is in progress
-    # - All tasks are completed: Workflow is completed
+    # - What happens if all of them are completed except one which is pending, should be in progress
     def on_task_state_changed(self):
-        if all(task._state == StateStatus.COMPLETED for task in self._tasks):
+        if all(task.state == StateStatus.COMPLETED for task in self._tasks):
             self._state = StateStatus.COMPLETED
-        if any(task._state == StateStatus.IN_PROGRESS for task in self._tasks):
+        elif any(task.state == StateStatus.IN_PROGRESS for task in self._tasks):
             self._state = StateStatus.IN_PROGRESS
+        elif all(task.state == StateStatus.PENDING for task in self._tasks):
+            self._state = StateStatus.PENDING
         else:
-            self._status = StateStatus.PENDING
+            self._state = StateStatus.IN_PROGRESS
 
 
 class Task:
 
-    def __init__(self, name:str, workflow_parent: WorkFlow):
+    def __init__(self, name: str, workflow_parent: WorkFlow):
         self.name = name
         self.workflow_parent = workflow_parent
         self.link = None
@@ -52,9 +52,30 @@ class Task:
     # Just ads one link to a task, maybe could be more than one link if this tasks triggers multiple tasks
     def add_link(self, link):
         self.link = link
-    
-    def change_state(self, new_state: StateStatus):
+
+    def change_task_state(self, new_state: StateStatus):
         self._state = new_state
         if self.workflow_parent:
             self.workflow_parent.on_task_state_changed()
-            
+
+        if self.link:
+            self.link.on_source_state_changed()
+
+
+class Link:
+
+    def __init__(
+        self,
+        source_task: Task,
+        trigger_state: StateStatus,
+        target_task: Task,
+        update_state: StateStatus,
+    ):
+        self.source_task = source_task
+        self.trigger_state = trigger_state
+        self.target_task = target_task
+        self.update_state = update_state
+
+    def on_source_state_changed(self):
+        if self.source_task.state == self.trigger_state:
+            self.target_task.change_task_state(self.update_state)
